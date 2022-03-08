a049bc673d16c8c2abdce374e528a2342da89e75
trudy19 <troudi.abderrazek@gmail.com>
2022-03-08 13:16:30 +0100

    first commit12

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  database: 'DATABASE_URL', // 'denso'
+  database: 'postgres', // 'denso'
    user: 'postgres',
    password: '0000',
-  host: 'postgres://vwdzwrtjxdledi:e7548369a6589d38066fb37d5078d94c17b60f30baec47ca6d60d612a72c0f3c@ec2-3-212-45-192.compute-1.amazonaws.com:5432/d3hfa82obsfdde',
+  host: 'localhost',
   port: 5432,
 }
